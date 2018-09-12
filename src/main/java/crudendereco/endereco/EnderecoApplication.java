2b2c00d968283e81b9f84fe102db95ba09adcc66
Daniel de Jesus Tavares <f520127@corp.caixa.gov.br>
2018-09-12 16:37:13 -0300

    Publicacao do projeto inicial

---
+++ src/main/java/crudendereco/endereco/EnderecoApplication.java
@@ -0,0 +1,12 @@
+package crudendereco.endereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnderecoApplication.class, args);
+	}
+}
